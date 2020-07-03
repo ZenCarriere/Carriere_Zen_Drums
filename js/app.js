6e5d248e90c66f6513043163a8820cfd0f8c15f6
ZenCarriere <59712949+ZenCarriere@users.noreply.github.com>
2020-07-03 12:36:33 -0400

    transform

--- js/app.js
+++ js/app.js
@@ -2,14 +2,33 @@
 
 	console.log('fire');
 
-	function playMatchingAudio(event) {
-		
-		let card1 = event.keyCode;
-		let card2 = document.querySelector(`audio[data-key="${card1}"]`);
+	let keys = Array.from(document.querySelectorAll('.key'));
 
-		if(card2) {card2.play(); }
+
+	function resetKey() {
+		this.classList.remove('playing');
+	}
+
+	function playMatchingAudio(event) {
+
+		let card1 = event.keyCode;
+		let card2 = document.querySelector(`audio[data-key="${card1}"]`),
+		key = document.querySelector(`div[data-key="${card1}"]`);
+
+		if(!card2) {return;}
+
+		if(card2) {
+
+			card2.currentTime = 0;
+
+			card2.play(); 
+
+			key.classList.add('playing');
+		}
 	}
 
 	window.addEventListener('keydown', playMatchingAudio);
+
+	keys.forEach(key => key.addEventListener('transitionend', resetKey));
 
 })();
